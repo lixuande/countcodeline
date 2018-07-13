8ab10994233a71f9cec2ccee8399c0a341361bbd
zhaotiejia <zhaotiejia@ebupt.com>
2018-07-13 11:22:16 +0800

    delete log file

---
+++ deletelogfile.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#---------------------------------------------
+#  Copyright(C) 2018 EASTCOM-BUPT Inc.
+#
+#  Author      : even li
+#  Description : even li at ebupt.com
+#  History     : 2018-06-28 Created
+#
+#---------------------------------------------
+
+g_count=0 
+function deletelogfile()
+{
+	filename=$1
+	fileformart=`echo ${filename##*.}`
+	if [ $fileformart = "log" ];
+	then
+		echo "-----rm----$filename-----"
+		let g_count+=1
+		rm $filename
+	fi
+}
+
+function findlogfile()
+{
+	for element in `ls $1`
+	do
+		dir_or_file=$1"/"$element
+		if [ -d $dir_or_file ];
+		then
+			findlogfile $dir_or_file
+		else
+			deletelogfile $dir_or_file
+		fi
+	done
+}
+
+codefiledir=$1
+findlogfile $codefiledir
+echo "----delete[$g_count]log file------" 
